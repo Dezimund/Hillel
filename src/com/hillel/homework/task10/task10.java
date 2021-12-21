05cab935d3caec2009e1ee7a8a82bca4c2596e9c
Dezimund <de3mond15@gmail.com>
2021-12-21 21:05:42 +0200

    Updated

--- src/com/hillel/homework/task10/task10.java
+++ src/com/hillel/homework/task10/task10.java
@@ -1,4 +1,29 @@
 package com.hillel.homework.task10;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class task10 {
+    public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static double number1 (double number){
+        return (number);
+
+    }
+
+    public static double number2 (double number2){
+        return (number2);
+    }
+
+   // public static char sign (char sign){
+      //  return Math.sign(+);
+   // }
+
+
 }
--- src/com/hillel/homework/task12/task12.java
+++ src/com/hillel/homework/task12/task12.java
@@ -1,4 +1,21 @@
 package com.hillel.homework.task12;
 
 public class task12 {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+
+            if (i % 10 == 4 || i % 100 == 4) {
+                continue;
+            } else if (i % 10 == 9 || i % 100 == 9) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+    }
 }
