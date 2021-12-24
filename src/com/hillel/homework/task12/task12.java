edfba25186be1163f358b37347a83eb79d73df87
Maxim Tantsiura <de3mond15@gmail.com>
2021-12-24 18:30:20 +0200

    Update task12.java

--- src/com/hillel/homework/task12/task12.java
+++ src/com/hillel/homework/task12/task12.java
@@ -1,3 +1,11 @@
+/*
+In Japan, numbers 4 and 9 are considered unlucky. You only need to output the lucky numbers between 0 and 100.
+
+It is necessary to exclude from the output all numbers that contain 4 and 9.
+
+(there should not be numbers 4 and 9, not only in the first ten, but also 14, 19, 24, 39, etc.)
+*/
+
 package com.hillel.homework.task12;
 
 public class task12 {
