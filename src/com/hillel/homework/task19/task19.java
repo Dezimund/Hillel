67f1804d17986d70e36dc0708e3526adc414bb92
Maxim Tantsiura <de3mond15@gmail.com>
2022-01-05 01:45:44 +0200

    Update task19.java

--- src/com/hillel/homework/task19/task19.java
+++ src/com/hillel/homework/task19/task19.java
@@ -1,4 +1,35 @@
 package com.hillel.homework.task19;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class task19 {
+
+    static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+
+        int[] mas = new int [5];
+
+        for (int i=0; i<mas.length; i++){
+
+            System.out.println("Input array :");
+            mas[i] = Integer.parseInt(READER.readLine());
+
+        }
+        Arrays.sort(mas);
+
+        System.out.println();
+
+        for (int i : mas) {
+            System.out.print(i + " ");
+        }
+    }
+
+
+
+
 }
