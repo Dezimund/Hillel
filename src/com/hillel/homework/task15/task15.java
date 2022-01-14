1f74f82c22aa43820a125a4e28a2d79cff3537a8
Maxim Tantsiura <de3mond15@gmail.com>
2022-01-14 19:40:32 +0200

    Update task15.java

--- src/com/hillel/homework/task15/task15.java
+++ src/com/hillel/homework/task15/task15.java
@@ -1,4 +1,38 @@
-package com.hillel.homework.task15;
+package homework.task15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 
 public class task15 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input Palindrome");
+        String text = reader.readLine();
+        text = text.replace(" ","");
+
+        String reverse = "";
+        for (int i = text.length() - 1; i >= 0 ; i--) {
+            reverse += text.charAt(i);
+            System.out.println(reverse);
+        }
+        boolean palindrome = true;
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) != reverse.charAt(i)){
+                palindrome = false;
+            }
+        }
+
+        if(palindrome){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not a palindrome");
+        }
+
+
+
+    }
 }
