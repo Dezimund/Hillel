ffa10113dfbce7da7205be03575b4af2177eb998
Maxim Tantsiura <de3mond15@gmail.com>
2022-01-22 16:13:12 +0200

    Add files via upload

---
+++ src/com/hillel/homework/task26/FamilyUsage.java
@@ -0,0 +1,23 @@
+package homework.task26;
+
+
+
+public class FamilyUsage {
+
+
+    public static void main(String[] args){
+
+        Family firstGeneration = new Family("Vasya","Anya","Alex");
+
+        Family secondGeneration = new Family("Miranda","Fiona","Richard");
+
+
+        System.out.println("First Generation is : " + firstGeneration);
+
+        System.out.println("Second Generation is : " + secondGeneration);
+
+
+
+
+    }
+}
